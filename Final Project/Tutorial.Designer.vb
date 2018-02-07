980a48b93d90267cdd17980a7120639b6ab51931
Kayla <kc125327@students.davenport.k12.ia.us>
2018-02-07 08:31:07 -0600

    update

--- Final Project/Tutorial.Designer.vb
+++ Final Project/Tutorial.Designer.vb
@@ -85,7 +85,7 @@ Partial Class Tutorial
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Location = New System.Drawing.Point(15, -30)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(1244, 739)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
